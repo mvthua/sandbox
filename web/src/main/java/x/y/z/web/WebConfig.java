a0ca10c01ed5eb39d50db92806d5c31b5477c833
Michael Hua <mvthua@gmail.com>
2012-08-15 16:17:56 +0800

    Simple web app using Java Config

---
+++ web/src/main/java/x/y/z/web/WebConfig.java
@@ -0,0 +1,41 @@
+/**
+ * WebConfig.java
+ */
+package x.y.z.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Michael
+ */
+@ComponentScan(basePackages = { "x.y.z.web.controller" })
+@Configuration
+@EnableWebMvc
+//@Import(DataConfig.class)
+//@ImportResource("classpath:/logging-ctx.xml")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resource/**").addResourceLocations("/resource/");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
+
+}
